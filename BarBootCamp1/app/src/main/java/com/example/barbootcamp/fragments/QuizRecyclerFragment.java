0c64ee17985244f81c2d55f26f9bf9fede7676ec
Joanna Damianakis <z5162567@ad.unsw.edu.au>
2019-11-21 22:07:27 +1100

    Attempt at marking quiz

--- BarBootCamp1/app/src/main/java/com/example/barbootcamp/fragments/QuizRecyclerFragment.java
+++ BarBootCamp1/app/src/main/java/com/example/barbootcamp/fragments/QuizRecyclerFragment.java
@@ -1,10 +1,14 @@
 package com.example.barbootcamp.fragments;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -58,10 +62,26 @@ public class QuizRecyclerFragment extends Fragment {
         }
         quizAdapter.setQuestionData(newQlist);
         recyclerView.setAdapter(quizAdapter);
+        submitBtn = view.findViewById(R.id.quizFragment_submitBtn);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for(Question q: questionList) {
+                    if(q.getTopicID().equals(topicId)) {
+                        RadioGroup radioGroup = (RadioGroup) view.findViewById(R.id.quiz_radioGroup);
+                        int selectedId = radioGroup.getCheckedRadioButtonId();
+                        int answer = q.getAnswer();
+                        if (selectedId == answer) {
+                            score += 1;
+                        } else {
+                            score += 0;
+                        }
+                    }
+                }
 
-
+                System.out.print("You got " + score + " in your quiz");
+            }
+        });
         return view;
     }
-
-
 }
