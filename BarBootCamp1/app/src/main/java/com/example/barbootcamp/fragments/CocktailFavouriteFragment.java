40fef9a78494d89aba79585963839fb5775867cc
Luna <z5045863@unsw.edu.au>
2019-11-21 22:02:56 +1100

    2

--- BarBootCamp1/app/src/main/java/com/example/barbootcamp/fragments/CocktailFavouriteFragment.java
+++ BarBootCamp1/app/src/main/java/com/example/barbootcamp/fragments/CocktailFavouriteFragment.java
@@ -1,6 +1,34 @@
 package com.example.barbootcamp.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.barbootcamp.R;
+import com.example.barbootcamp.model.Cocktail;
+
+import java.util.ArrayList;
 
 public class CocktailFavouriteFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    public static ArrayList<Cocktail>favCocktails = new ArrayList<>();
+
+    public CocktailFavouriteFragment(){
+
+    }
+
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_cocktailfavourite_recycler, container, false);
+
+        recyclerView = view.findViewById(R.id.favourite_rv);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(view.getContext());
+        return view;
+    }
 }
